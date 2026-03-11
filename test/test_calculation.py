7c3fc835555153598b0fe2b540171f4ca469f07f
Fazal-Kochi <shaikhfazal346@gmail.com>
2026-03-11 20:07:42 +0530

    Add tests for add and subtract functions
    Fix import statements and add test cases for add and subtract functions.

---
+++ test/test_calculation.py
@@ -0,0 +1,15 @@
+from app.calculatior import add,substract
+
+'''
+from aap -> folder
+calculator -> file name
+import add,substract (functions)
+'''
+
+def test_add():
+  assert add (2,3) == 5
+
+
+def test_substract():
+  assert sy=ubstract(5,3) == 2
+  
