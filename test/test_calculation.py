c8597f3959162a4c2d0d417f6f4d03b82a5a770e
Fazal-Kochi <shaikhfazal346@gmail.com>
2026-03-12 16:45:56 +0530

    Update test_calculation.py

--- test/test_calculation.py
+++ test/test_calculation.py
@@ -6,10 +6,9 @@ calculator -> file name
 import add,substract (functions)
 '''
 
-def test_add():
-  assert add (2,3) == 5
+def subtract(a, b):
+    return a - b
 
-
-def test_substract():
-  assert sy=ubstract(5,3) == 2
+def test_subtract():
+    assert subtract(5, 3) == 2
   
